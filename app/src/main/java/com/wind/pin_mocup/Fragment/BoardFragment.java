ec6c371e8ca7b529c113a2b758053b07dd8127fb
farmstyle <farmstyle1@gmail.com>
2016-02-25 21:38:57 +0700

    edit

---
+++ app/src/main/java/com/wind/pin_mocup/Fragment/BoardFragment.java
@@ -0,0 +1,7 @@
+package com.wind.pin_mocup.Fragment;
+
+/**
+ * Created by Miki on 2/25/2016.
+ */
+public class BoardFragment {
+}
--- app/src/main/java/com/wind/pin_mocup/MainActivity.java
+++ app/src/main/java/com/wind/pin_mocup/MainActivity.java
@@ -13,16 +13,16 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import Library.ProfilePictureView;
+
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-        private ProfilePictureView profilePicture;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("Friend");
+        setTitle("");
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
